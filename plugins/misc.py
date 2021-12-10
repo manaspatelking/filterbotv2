ec74a6307511374be6046ff802d61f93b3e943f2
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-10 18:22:08 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -16,21 +16,21 @@ async def showid(client, message):
         username = message.from_user.username
         dc_id = message.from_user.dc_id or ""
         await message.reply_text(
-            f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> {username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n<b>➲ Data Centre:</b> <code>{dc_id}</code>",
+            f"<b>👤 First Name:</b> {first}\n<b>🗣️ Last Name:</b> {last}\n<b>👮 Username:</b> {username}\n<b>🚸 Telegram ID:</b> <code>{user_id}</code>\n<b>🌐 Data Centre:</b> <code>{dc_id}</code>",
             quote=True
         )
 
     elif chat_type in ["group", "supergroup"]:
         _id = ""
         _id += (
-            "<b>➲ Chat ID</b>: "
+            "<b>💢 Chat ID 💢</b>: "
             f"<code>{message.chat.id}</code>\n"
         )
         if message.reply_to_message:
             _id += (
-                "<b>➲ User ID</b>: "
+                "<b>👮 User ID</b>: "
                 f"<code>{message.from_user.id}</code>\n"
-                "<b>➲ Replied User ID</b>: "
+                "<b>▶ Replied User ID</b>: "
                 f"<code>{message.reply_to_message.from_user.id}</code>\n"
             )
             file_info = get_file_id(message.reply_to_message)
@@ -70,15 +70,15 @@ async def who_is(client, message):
         await status_message.edit("no valid user_id / message specified")
     else:
         message_out_str = ""
-        message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
+        message_out_str += f"<b>👤First Name:</b> {from_user.first_name}\n"
         last_name = from_user.last_name or "<b>None</b>"
-        message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
-        message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
+        message_out_str += f"<b>🗣️Last Name:</b> {last_name}\n"
+        message_out_str += f"<b>🚸Telegram ID:</b> <code>{from_user.id}</code>\n"
         username = from_user.username or "<b>None</b>"
         dc_id = from_user.dc_id or "[User Doesnt Have A Valid DP]"
-        message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
-        message_out_str += f"<b>➲User Name:</b> @{username}\n"
-        message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
+        message_out_str += f"<b>🌐Data Centre:</b> <code>{dc_id}</code>\n"
+        message_out_str += f"<b>🌀User Name:</b> @{username}\n"
+        message_out_str += f"<b>♂️User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
         if message.chat.type in (("supergroup", "channel")):
             try:
                 chat_member_p = await message.chat.get_member(from_user.id)
